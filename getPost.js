fec003073d936d88e3afc59553a646511f9397e2
Dav-lopez <78981910+Dav-lopez@users.noreply.github.com>
2021-04-18 23:14:56 +0100

    Add files via upload

---
+++ getPost.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+//const http = require ('https');
+const fetch = require('node-fetch');
+
+// fetching data from JSON Placeholder post
+
+let url = "http://jsonplaceholder.typicode.com/posts";
+
+let settings = { method: "Get" };
+
+ fetch(url, settings)
+    .then(res => res.json())
+    // to write file to post.json 
+    .then(json=> { fs.writeFile("./result/post.json", JSON.stringify(json), err => {
+        // Check file writing operation status
+        if (err) throw err;
+        // Log status of wiriting operation to console
+        console.log("File writing is done successfully")
+    })}
+    );
+
+
+    
+
+   
+
+   
